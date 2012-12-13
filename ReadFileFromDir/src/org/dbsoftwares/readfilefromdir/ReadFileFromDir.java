e111c71296da257b09f7bbaa8d8613171e26ffb8
Péter György Tóth <tothpetergyorgy@gmail.com>
2012-12-13 03:39:17 +0100

    Release commit
    Ugly but can do the job.
    Next release will use FileInputStream

--- ReadFileFromDir/src/org/dbsoftwares/readfilefromdir/ReadFileFromDir.java
+++ ReadFileFromDir/src/org/dbsoftwares/readfilefromdir/ReadFileFromDir.java
@@ -1,43 +1,38 @@
 package org.dbsoftwares.readfilefromdir;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
  *
- * @author Tóth Péter György (ptoth @ github.com)
- * @author Deniel Buzas (dbuzas @ github.com) 
+ * @author Tóth Péter György (github.com/ptoth)
+ * @author Deniel Buzas (github.com/dbuzas)
  * @since 2012.12.13.
  */
-
 public class ReadFileFromDir {
 
     public static void main(String[] args) {
         System.out.println("Hi Den!");
         //init scanner for input data:
         Scanner input = new Scanner(System.in);
-        
-        /**
-         * Get the full path and the filename
-         * example:
-         * /Users/Den/
-         * log.txt
-         */
-//        System.out.println("I need a directory absolute path first:");
-        System.out.println("Give me the file's name then:");
-        String dirPath = input.nextLine();
-        File specifiedDirectory = new File(dirPath);
-//        
 
-        if (!specifiedDirectory.exists() || !specifiedDirectory.canRead()) {
-            System.err.println("There is no such dir or access to it!");
-        } else{
-            System.out.println("Directory access granted!");
-            //System.out.println(dirPath.getAbsolutePath());
-            //File specifiedFile = new File(fileName);
-            
-            //System.out.println(specifiedFile.getParent());
+        /**
+         * Get the full path example: C:\Users\Den\log.txt
+         */
+        System.out.println("Give me the file's abs. path:");
+        String dirPath = input.nextLine();
+        File fileToRead = new File(dirPath);
+
+        try {
+            Scanner inputFile = new Scanner(fileToRead);
+            while (inputFile.hasNextLine()) {
+                String line = inputFile.nextLine();
+                System.out.println(line);
+            }
+            inputFile.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
